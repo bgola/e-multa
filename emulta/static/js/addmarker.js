eeb137bc7ecad26f802f93e0f89f2d2acc9309dd
Bruno Gola <brunogola@gmail.com>
2011-06-15 15:39:28 -0300

    zzZzZ

--- emulta/static/js/addmarker.js
+++
@@ -1,9 +0,0 @@
-var marker = null;
-function addmarker(e) {
-	var map = $('div.gmap:visible');
-	if (!marker) {
-		marker = new google.maps.Marker({position: e.latLng, map: map.getMap()});
-	} 
-	marker.setPosition(e.latLng);
-	$('#id_location').val(e.latLng);
-}
